e37b6af4229b4c662ab9a820d85ae21e02790294
giilais <giovanna.laisoliveiraa@gmail.com>
2021-12-07 20:27:08 -0300

    Finalização Pagina Cadastro de Produto

--- src/Pages/CadastroProduto/CadastroProduto.jsx
+++ src/Pages/CadastroProduto/CadastroProduto.jsx
@@ -20,6 +20,8 @@ import { SmallCloseIcon } from '@chakra-ui/icons';
 
 import { useState } from 'react';
 import { axiosPost } from '../../Service/service';
+import Header from '../../Components/Header';
+import Footer from '../../Components/Footer';
 
 const CadastroProduto = () => {
 
@@ -49,70 +51,76 @@ const CadastroProduto = () => {
 
     return(
         <ChakraProvider>
-             <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack
-        spacing={4}
-        w={'full'}
-        maxW={'md'}
-        bg={useColorModeValue('white', 'gray.700')}
-        rounded={'xl'}
-        boxShadow={'lg'}
-        p={6}
-        my={12}>
-        <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-          Cadastro de Produto
-        </Heading>
-        <FormControl id="userName">
-          <FormLabel>Foto Produto</FormLabel>
-          <Stack direction={['column', 'row']} spacing={6}>
-            <Center>
-              <Avatar size="xl" src="">
-              </Avatar>
-            </Center>
-            <Center w="full">
-              <Button w="full">Mudar Foto</Button>
-            </Center>
-          </Stack>
-        </FormControl>
-        <FormControl>
-            <FormLabel>Marca do Produto</FormLabel>
-            <Input value={Marca} onInput={e => setMarca(e.target.value)} type="text"/>
-        </FormControl>
-        <FormControl>
-            <FormLabel>Preço</FormLabel>
-            <Input value={Preco} onInput={e => setPreco(e.target.value)} type="number"/>
-        </FormControl>
-        <FormControl>
-            <FormLabel>Descrição</FormLabel>
-            <Textarea value={Descricao} onInput={e => setDescricao(e.target.value)} />
-        </FormControl>
-        <Stack spacing={6} direction={['column', 'row']}>
-          <Button
-            bg={'red.500'}
-            color={'white'}
-            w="full"
-            _hover={{
-              bg: 'red.500',
-            }}>
-            Cancelar
-          </Button>
-          <Button
-            onClick={clicked}
-            bg={'Black'}
-            color={'white'}
-            w="full"
-            _hover={{
-              bg: 'gray.600',
-            }}>
-            Enviar
-          </Button>
-        </Stack>
-      </Stack>
-    </Flex>
+
+          <Header/>
+
+          <Flex
+            minH={'10vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack
+              spacing={4}
+              w={'full'}
+              maxW={'md'}
+              bg={useColorModeValue('white', 'gray.700')}
+              rounded={'xl'}
+              boxShadow={'lg'}
+              p={6}
+              my={12}>
+              <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+                Cadastro de Produto
+              </Heading>
+              <FormControl id="userName">
+                <FormLabel>Foto Produto</FormLabel>
+                <Stack direction={['column', 'row']} spacing={6}>
+                  <Center>
+                    <Avatar size="xl" src="">
+                    </Avatar>
+                  </Center>
+                  <Center w="full">
+                    <Button w="full">Mudar Foto</Button>
+                  </Center>
+                </Stack>
+              </FormControl>
+              <FormControl>
+                  <FormLabel>Marca do Produto</FormLabel>
+                  <Input value={Marca} onInput={e => setMarca(e.target.value)} type="text"/>
+              </FormControl>
+              <FormControl>
+                  <FormLabel>Preço</FormLabel>
+                  <Input value={Preco} onInput={e => setPreco(e.target.value)} type="number"/>
+              </FormControl>
+              <FormControl>
+                  <FormLabel>Descrição</FormLabel>
+                  <Textarea value={Descricao} onInput={e => setDescricao(e.target.value)} />
+              </FormControl>
+              <Stack spacing={6} direction={['column', 'row']}>
+                <Button
+                  bg={'red.400'}
+                  color={'white'}
+                  w="full"
+                  _hover={{
+                    bg: 'red.500',
+                  }}>
+                  Cancelar
+                </Button>
+                <Button
+                  onClick={clicked}
+                  bgGradient="linear(to-r, red.600,pink.600)"
+                  color={'white'}
+                  w="full"
+                  _hover={{
+                    bg: 'linear(to-r, red.200,pink.200)',
+                  }}>
+                  Enviar
+                </Button>
+              </Stack>
+            </Stack>
+          </Flex>
+
+          <Footer/>
+
         </ChakraProvider>
     );
 }
