b63df28d3bbc00d0fd592327614c4cb66cd8c4f3
RenatoJunior <rrenatosilva.rs@gmail.com>
2021-12-07 20:52:42 -0300

    Precisa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react'; // Precisa do ChakraProvider
 // import { render } from '@testing-library/react';
-// import Login from '../src/Pages/Login/Login';
-import Cadastro from '../src/Pages/Cadastro/Cadastro';
+import Login from '../src/Pages/Login/Login';
+// import Cadastro from '../src/Pages/Cadastro/Cadastro';
 // import Buy from '../src/Pages/Buy/Buy';
 // import Perfil from '../src/Pages/Perfil/Perfil';
 // import CadastroProduto from '../src/Pages/CadastroProduto/CadastroProduto';
@@ -9,10 +9,13 @@ import Cadastro from '../src/Pages/Cadastro/Cadastro';
 // import Pay from '../src/Pages/Pay/Pay'
 
 function App() {
+    // Precisa do ChakraProvider
+    return (
+        <ChakraProvider>
+            <Login/>
+        </ChakraProvider>
 
-  return (
-  <Cadastro/>
-  );
+    );
 }
 
 export default App;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -13,18 +13,20 @@ import {
     Heading,
     Text,
     useColorModeValue,
-  } from '@chakra-ui/react';
+    useToast
+} from '@chakra-ui/react';
 import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
 
-  import { useState } from 'react';
-  import { axiosPost } from '../../Service/service';
+import { useState } from 'react';
+import { axiosPost } from '../../Service/service';
 
 const Login = () => {
     const [Usuario, setUsuario] = useState('');
     const [Senha, setSenha] = useState('');
 
     const link = 'http://localhost:3001/v1/public/login';
+    const toast = useToast();
     const clicked = async ()=>{
         try{
             const resposta = await axiosPost(
@@ -37,69 +39,77 @@ const Login = () => {
             localStorage.setItem('autentication', JSON.stringify(resposta));
             console.log(resposta);
         }catch(ex){
+            toast({
+              title: 'Dados inválidos',
+              description: "Verifique se os dados de acesso são válidos",
+              position: 'top-left',
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+            });
             console.error(ex);
         }
     }
 
-     return(
+    return(
         <ChakraProvider>
 
-          <Header/>
-          <Flex
-            minH={'65vh'}
-            align={'center'}
-            justify={'center'}
-            bg={useColorModeValue('gray.50', 'gray.800')}
-          >
+        <Header/>
+        <Flex
+        minH={'65vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}
+        >
 
         <Stack spacing={8} mx={'auto'} width={'500px'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading>Seja bem-vindo(a)</Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              faça seu login abaixo
-            </Text>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email ou Username</FormLabel>
-              <Input value={Usuario} onInput={e => setUsuario(e.target.value)} type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Senha</FormLabel>
-              <Input value={Senha} onInput={e => setSenha(e.target.value)} type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Continuar conectado</Checkbox>
-                <Link color={'blue.400'}>Esqueceu a senha?</Link>
-              </Stack>
+        <Stack align={'center'}>
+        <Heading>Seja bem-vindo(a)</Heading>
+        <Text fontSize={'lg'} color={'gray.600'}>
+        faça seu login abaixo
+        </Text>
+        </Stack>
+        <Box
+        rounded={'lg'}
+        bg={useColorModeValue('white', 'gray.700')}
+        boxShadow={'lg'}
+        p={8}>
+        <Stack spacing={4}>
+        <FormControl id="email">
+        <FormLabel>Email ou Username</FormLabel>
+        <Input value={Usuario} onInput={e => setUsuario(e.target.value)} type="email" />
+        </FormControl>
+        <FormControl id="password">
+        <FormLabel>Senha</FormLabel>
+        <Input value={Senha} onInput={e => setSenha(e.target.value)} type="password" />
+        </FormControl>
+        <Stack spacing={10}>
+        <Stack
+        direction={{ base: 'column', sm: 'row' }}
+        align={'start'}
+        justify={'space-between'}>
+        <Checkbox>Continuar conectado</Checkbox>
+        <Link color={'blue.400'}>Esqueceu a senha?</Link>
+        </Stack>
 
-              <Button
-                onClick={clicked}
-                bgGradient="linear(to-r, red.400,pink.400)"
-                color={'white'}
-                _hover={{
-                  bgGradient: 'linear(to-r, red.400,pink.400)',
-                  boxShadow: 'xl',
-                }}>
-                Entrar
-              </Button>
-            </Stack>
-          </Stack>
+        <Button
+        onClick={clicked}
+        bgGradient="linear(to-r, red.400,pink.400)"
+        color={'white'}
+        _hover={{
+            bgGradient: 'linear(to-r, red.400,pink.400)',
+            boxShadow: 'xl',
+        }}>
+        Entrar
+        </Button>
+        </Stack>
+        </Stack>
         </Box>
-      </Stack>
-    </Flex>
+        </Stack>
+        </Flex>
 
-    <Footer/>
-</ChakraProvider>  
+        <Footer/>
+        </ChakraProvider>
     );
 };
 
